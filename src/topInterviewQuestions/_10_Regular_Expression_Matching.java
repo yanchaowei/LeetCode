a2866a9c2d11b7fd77fdfa495504e7c3496b1e99
闫超威 <yanchaowei@yanchaoweideMacBook-Pro.local>
2020-08-10 00:29:35 +0800

    44

--- src/topInterviewQuestions/_10_Regular_Expression_Matching.java
+++ src/topInterviewQuestions/_10_Regular_Expression_Matching.java
@@ -24,4 +24,29 @@ public class _10_Regular_Expression_Matching {
         boolean match = regular_expression_matching.isMatch(s, p);
         System.out.println(match);
     }
+
+    // 2D dp
+    public boolean isMatch2(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for(int i = 0; i < p.length(); i++) {
+            if(p.charAt(i) == '*' && dp[0][i - 1]) {
+                dp[0][i + 1] = true;
+            }
+        }
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = 0; j < p.length(); j++) {
+                if(p.charAt(j) == s.charAt(i) || p.charAt(j) == '.')
+                    dp[i + 1][j + 1] = dp[i][j];
+                if(p.charAt(j) ==  '*')
+                    if(j > 0 && (p.charAt(j - 1) == s.charAt(i) || p.charAt(j - 1) == '.'))
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i + 1][j - 1] || dp[i][j + 1]);
+                    else dp[i + 1][j + 1] = dp[i + 1][j - 1];
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
 }
